f19433482fe0aec7321a1544c934f20323a0ff81
danielkiss <kissdani89@gmail.com>
2015-11-07 19:20:11 +0100

    * Chat_Xamarin_GUI.csproj: GTK client
    * Program.cs:
    * MainWindow.cs:
    * gui.stetic:
    * generated.cs:
    * MainWindow.cs:
    * AssemblyInfo.cs:

---
+++ Chat_Xamarin_GUI/MainWindow.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow: Gtk.Window
+{
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+		
+	protected void Exit (object sender, EventArgs e)
+	{
+		Application.Quit ();
+	}
+
+	protected void handleButton (object sender, EventArgs e)
+	{
+		Send ();
+		this.chatField.GrabFocus ();
+	}
+
+	protected void handleEnterPressed (object sender, EventArgs e)
+	{
+		Send ();
+	}
+
+	public void Send(){
+		var text = this.chatField.Text + '\n';
+		this.chatField.Text = "";
+
+		this.chatLog.Buffer.InsertAtCursor (text);
+	}
+}
